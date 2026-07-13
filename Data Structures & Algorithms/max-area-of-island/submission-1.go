1e516a436678411f8194f59ee741221f472d3004
Tiotsyo <119519856+tiotsyodev@users.noreply.github.com>
2026-07-13 18:48:22 +0300

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.go
@@ -0,0 +1,36 @@
+func maxAreaOfIsland(grid [][]int) int {
+    rows := len(grid)
+	cols := len(grid[0])
+
+	res := 0
+	cur := 0
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			dfs(grid, &cur, rows, cols, i, j)
+
+			if cur > res {
+				res = cur
+			}
+
+			cur = 0
+		}	
+	}
+
+	return res
+}	
+
+func dfs(grid [][]int, cur *int, rows, cols, i, j int) {
+	if i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] == 0 {
+		return
+	}
+
+	*cur += 1
+
+	grid[i][j] = 0
+
+	dfs(grid, cur, rows, cols, i+1, j)
+	dfs(grid, cur, rows, cols, i, j+1)
+	dfs(grid, cur, rows, cols, i, j-1)
+	dfs(grid, cur, rows, cols, i-1, j)
+}
